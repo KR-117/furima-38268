28088ed3ff1b4425ca3716a14d7200267ce1c13e
KR-117 <inzaghi3303@aol.jp>
2022-07-28 20:18:46 +0900

    Basic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auch
+
+  private
+
+  def basic_auch
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 end
