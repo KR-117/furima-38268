e0910eea38d0c2115c5d1f1c1a59ebc2ed134868
KR-117 <inzaghi3303@aol.jp>
2022-08-03 15:11:53 +0900

    ActiveStorage及びActiveHashの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,8 @@
 class ItemsController < ApplicationController
 
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :text, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :shipment_date_id, :price).merge(user_id: current_user.id)
+  end
 end
