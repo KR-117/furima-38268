98adeba458f4abaca2187aedb34ea05a0240153c
KR-117 <inzaghi3303@aol.jp>
2022-08-08 14:30:06 +0900

    Formオブジェクト及び一部カラム名の変更

--- README.md
+++ README.md
@@ -59,8 +59,8 @@
 | postal_code   | string     | null: false                    |
 | prefecture_id | integer    | null: false                    |
 | city          | string     | null: false                    |
-| address       | string     | null: false                    |
-| apartment     | string     |                                |
+| house_number  | string     | null: false                    |
+| building_name | string     |                                |
 | phone_number  | string     | null: false                    |
 
 ### Association
---
+++ app/models/purchase_address.rb
@@ -0,0 +1,19 @@
+class PurchaseAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :city
+    validates :house_number
+    validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: "is invalid" }
+  end
+
+  def save
+    purchase = Purchase.create()
+    Address.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number)
+  end
+end
